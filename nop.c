58f74a4a35d17db3046237a68a16cb7bb77e40cc
Casopia-Beki <bereketkassaye@gmail.com>
2023-04-14 23:01:56 -0700

    Stacks, Queues - LIFO, FIFO

---
+++ nop.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+/**
+ * _nop - doesn’t do anything.
+ * @stack: head
+ * @num_line: number of the line
+ */
+void _nop(stack_t **stack, unsigned int num_line)
+{
+	if (*stack)
+	{};
+	if (num_line)
+	{};
+}
+#include "monty.h"
+
+/**
+ * _nop - doesn’t do anything.
+ * @stack: head
+ * @num_line: number of the line
+ */
+void _nop(stack_t **stack, unsigned int num_line)
+{
+	if (*stack)
+	{};
+	if (num_line)
+	{};
+}
